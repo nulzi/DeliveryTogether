bd7f36e360e969a0d77e74eb8ad72aaa433cd0f7
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-11-28 19:14:17 +0900

    [Create] project
    - vue cli3
    - vue router
    - vue pwa
    - vuex

---
+++ src/components/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav_container">
+    <nav class="nav_container">
+      <div class="home_btn">
+        <router-link to="/"
+          ><img src="../assets/home.png" alt=""
+        /></router-link>
+      </div>
+      <div class="user_btn">
+        <router-link to="/"
+          ><img src="../assets/user-circle.png" alt=""
+        /></router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav_container {
+  display: flex;
+  background-color: #ffcc33;
+}
+.user_btn {
+  display: flex;
+  margin-left: auto;
+}
+</style>
