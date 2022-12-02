56a1ad7d0617070c21b214d1e0334e13fd29a3f3
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-12-02 16:53:05 +0900

    [Add] MainView.vue
    -components: navBar, post, locationTag
    -action: home, user, add post

---
+++ src/views/MainView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mainView_container">
+    <navBar></navBar>
+    <locationTag></locationTag>
+    <div class="mainView_post_container">
+      <router-link to="/post">
+        <post></post>
+      </router-link>
+    </div>
+    <div class="mainView_floatingButton_container">
+      <router-link to="/image">
+        <img
+          class="mainView_floatingButton_button"
+          src="../assets/add-button.png"
+          alt=""
+        />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import navBar from '../components/navBar.vue';
+import locationTag from '../components/locationTag.vue';
+import post from '../components/post.vue';
+
+export default {
+  name: 'main',
+  components: {
+    navBar,
+    locationTag,
+    post,
+  },
+};
+</script>
+
+<style>
+.mainView_post_container {
+  margin-left: 12px;
+}
+
+.mainView_floatingButton_container {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+}
+.mainView_floatingButton_button {
+  background-color: #ffcc33;
+  width: 27px;
+  height: 27px;
+  border-radius: 14px;
+}
+</style>
