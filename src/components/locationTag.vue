17609778014b07ee8fac10c75d0f0eb3924bbcb5
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-12-02 16:59:11 +0900

    [Add] locationTag.vue
    component which show tag

---
+++ src/components/locationTag.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="locationTag_container">
+    <img src="../assets/hashtag.png" alt="" />제 1기숙사
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.locationTag_container {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  display: flex;
+  align-items: center;
+  margin: 14px 0px 10px 8px;
+  color: rgba(98, 59, 42, 0.8);
+}
+</style>
